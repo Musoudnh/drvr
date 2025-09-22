d425ea2d5a9163d395b04f46c505dfe30ee96482
Musoudnh <83262885+Musoudnh@users.noreply.github.com>
2025-09-23 03:30:43 +0500

    Updated TasksProjects.tsx

--- src/pages/Tasks/TasksProjects.tsx
+++ src/pages/Tasks/TasksProjects.tsx
@@ -602,6 +602,17 @@ const TasksProjects: React.FC = () => {
                     >
                       <List className="w-4 h-4 mr-1 inline" />
                       List
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              {/* Task View */}
+              {viewMode === 'board' ? renderKanbanBoard() : renderListView()}
+            </div>
+          )}
+
+          {activeTab === 'monday' && renderIntegrationTab('monday')}
           {connectedBoards.map(board => (
             activeTab === board.id && (
               <div key={board.id}>
