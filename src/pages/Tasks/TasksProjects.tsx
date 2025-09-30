dc6a8b2548310673d6f594d43ce2b2279fd29019
Musoudnh <83262885+Musoudnh@users.noreply.github.com>
2025-10-01 04:31:14 +0500

    Updated bundle-stats.html

--- src/pages/Tasks/TasksProjects.tsx
+++ src/pages/Tasks/TasksProjects.tsx
@@ -291,20 +291,6 @@ const TasksProjects: React.FC = () => {
             </div>
           </div>
           
-          {task.tags.length > 0 && (
-            <div className="flex flex-wrap gap-1 mt-2">
-              {task.tags.slice(0, 2).map(tag => (
-                <span key={tag} className="px-2 py-1 bg-[#3AB7BF]/10 text-[#3AB7BF] rounded text-xs">
-                  {tag}
-                </span>
-              ))}
-              {task.tags.length > 2 && (
-                <span className="px-2 py-1 bg-gray-100 text-gray-600 rounded text-xs">
-                  +{task.tags.length - 2}
-                </span>
-              )}
-            </div>
-          )}
           
           {/* View Details Button */}
           <button
