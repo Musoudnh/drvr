9d02dff941160cd3d019cf47c4ceea6b8147e957
Musoudnh <83262885+Musoudnh@users.noreply.github.com>
2025-10-02 07:05:45 +0500

    Updated bundle-stats.html

--- src/pages/Tasks/TasksProjects.tsx
+++ src/pages/Tasks/TasksProjects.tsx
@@ -1046,7 +1046,7 @@ const TasksProjects: React.FC = () => {
                         setIsEditingTask(false);
                       }
                     }}
-                    className="px-4 py-2 bg-[#3AB7BF] text-white rounded-lg hover:bg-[#2A9BA3] transition-colors"
+                    className="px-4 py-2 bg-[#212B36] text-white rounded-lg hover:bg-[#101010] transition-colors"
                   >
                     Save Changes
                   </button>
