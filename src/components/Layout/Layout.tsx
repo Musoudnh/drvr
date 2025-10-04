4ad35fedac6f00c259d75502f3a6762854b39190
Musoudnh <83262885+Musoudnh@users.noreply.github.com>
2025-10-05 01:44:39 +0500

    Updated bundle-stats.html

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -19,9 +19,9 @@ const Layout: React.FC<LayoutProps> = ({ title, children }) => {
     <div className="flex h-screen bg-white relative">
       <SkipLink href="#main-content">Skip to main content</SkipLink>
       <Sidebar isCollapsed={sidebarCollapsed} onToggle={() => setSidebarCollapsed(!sidebarCollapsed)} />
-      
+
       <div className="flex-1 flex flex-col overflow-hidden">
-        <TopNav title={title} />
+        <TopNav title={title} onToggleSidebar={() => setSidebarCollapsed(!sidebarCollapsed)} />
         
         <main id="main-content" className="flex-1 overflow-auto px-6 py-6">
           {children}
