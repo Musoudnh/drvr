6c54ab96a4714aa932cc522626523ee8b0bf3239
Musoudnh <83262885+Musoudnh@users.noreply.github.com>
2025-10-08 04:35:49 +0500

    Updated bundle-stats.html

--- src/components/Layout/TopNav.tsx
+++ src/components/Layout/TopNav.tsx
@@ -96,15 +96,13 @@ const TopNav: React.FC<TopNavProps> = ({ onToggleSidebar, title, onOpenViewSetti
         
         <div className="flex items-center space-x-3">
           {/* View Settings Filter */}
-          {onOpenViewSettings && (
-            <button
-              onClick={onOpenViewSettings}
-              className="p-1.5 text-gray-600 hover:text-[#101010] transition-colors rounded-lg hover:bg-gray-100"
-              title="View Settings"
-            >
-              <Filter className="w-4 h-4" />
-            </button>
-          )}
+          <button
+            onClick={onOpenViewSettings}
+            className="p-1.5 text-gray-600 hover:text-[#101010] transition-colors rounded-lg hover:bg-gray-100"
+            title="View Settings"
+          >
+            <Filter className="w-4 h-4" />
+          </button>
 
           {/* Notifications */}
           <div className="relative" ref={notificationRef}>
