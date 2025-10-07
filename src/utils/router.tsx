220e2732bad71edcf05e2b111d0405898c30de98
Musoudnh <83262885+Musoudnh@users.noreply.github.com>
2025-10-08 04:17:34 +0500

    Updated bundle-stats.html

--- src/utils/router.tsx
+++ src/utils/router.tsx
@@ -42,7 +42,6 @@ import WorkflowManagement from '../pages/Collaboration/WorkflowManagement';
 import PredictiveAnalytics from '../pages/Analytics/PredictiveAnalytics';
 import AuditTrail from '../pages/Compliance/AuditTrail';
 import TasksProjects from '../pages/Tasks/TasksProjects';
-import Sandbox from '../pages/Financials/Sandbox';
 import RoadMap from '../pages/RoadMap/RoadMap';
 import Approvals from '../pages/RoadMap/Approvals';
 import { Database, GitBranch, Brain } from 'lucide-react';
@@ -272,14 +271,6 @@ export const router = createBrowserRouter([
     element: (
       <ProtectedRoute title="Variance & Insights">
         <VarianceInsights />
-      </ProtectedRoute>
-    )
-  },
-  {
-    path: "/financials/sandbox",
-    element: (
-      <ProtectedRoute title="Financial Sandbox">
-        <Sandbox />
       </ProtectedRoute>
     )
   },
